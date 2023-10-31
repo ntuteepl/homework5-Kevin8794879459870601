a69b68015131f724b17e040eb177a9941f1afaef
Kevin8794879459870601 <146801042+Kevin8794879459870601@users.noreply.github.com>
2023-10-31 14:36:52 +0800

    Update hw5-3.c

--- hw5-3.c
+++ hw5-3.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 
 int main() {
-    float a, b;
-    scanf("%f%f",&a,&b);
+    int a, b;
+    scanf("%d%d",&a,&b);
     if (a<=60){
-            float c;
+            double c;
             c=a*b;
         printf("%.1f\n",c);
     }
     else if (a<=120){
-            float c;
+            double c;
             c=60*b+(a-60)*b*1.33;
         printf("%.1f\n",c);
     }
     else if (a>120){
-            float c;
+            double c;
             c=60*b+60*b*1.33+(a-120)*b*1.66;
         printf("%.1f\n",c);
     }
