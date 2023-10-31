48aee4355cf9f5324c15ece305731db9256d6198
Kevin8794879459870601 <146801042+Kevin8794879459870601@users.noreply.github.com>
2023-10-31 13:32:00 +0800

    Update hw5-1.c

--- hw5-1.c
+++ hw5-1.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    float celsius, fahrenheit;
+
+    // 提示用戶輸入攝氏溫度
+
+    scanf("%f", &celsius);
+
+    // 轉換攝氏溫度為華氏溫度
+    fahrenheit = (celsius * 9/5) + 32;
+
+    // 顯示轉換結果
+    printf("%.1f\n", fahrenheit);
+
+    return 0;
+}
 
