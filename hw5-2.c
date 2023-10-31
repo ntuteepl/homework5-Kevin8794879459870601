633fcab31b654a45936ea049a4568169e8267206
Kevin8794879459870601 <146801042+Kevin8794879459870601@users.noreply.github.com>
2023-10-31 13:46:21 +0800

    Update hw5-2.c

--- hw5-2.c
+++ hw5-2.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    float a, b;
+    scanf("%f",&a);
+    if (a<800){
+            float c;
+            c=a*9/10;
+        printf("%.1f\n",c);
+    }
+    else if (a<1500){
+            float c;
+            c=a*9*9/10/10;
+        printf("%.1f\n",c);
+    }
+    else if (a<1500){
+            float c;
+            c=a*9*79/1000;
+        printf("%.1f\n",c);
+    }
+
+
+
+    return 0;
+}
 
