bf097173ba8c1935bc6025c1cafaa378a75ad12b
Kevin8794879459870601 <146801042+Kevin8794879459870601@users.noreply.github.com>
2023-10-31 14:07:03 +0800

    Update hw5-4.c

--- hw5-4.c
+++ hw5-4.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    float a, b;
+    scanf("%f%f",&a,&b);
+    if ((a*30+b/2)>b*6){
+            float c;
+            c=(a*30+b/2)-b*6;
+            if (c>180){c=c-180;}
+        printf("%.3f\n",c);
+
+    }
+    else if ((a*30+b/2)<b*6){
+            float c;
+            c=b*6-(a*30+b/2);
+            if (c>180){c=c-180;}
+        printf("%.3f\n",c);
+    }
+
+
+    return 0;
+}
 
